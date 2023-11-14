eab3b676ead676bfcb3851fa6079a408e5e3358d
RanVargas <Ren-elias@hotmail.com>
2023-11-14 03:17:32 -0400

    First section "finished"

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className=" border-gray-200 bg-gray-900">
+    <nav className=" bg-[#0E0B1A]">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
@@ -34,11 +34,11 @@ function Navbar() {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 group">
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent"
                 aria-current="page"
               >
                 Home
@@ -47,7 +47,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-white rounded [&:not(:hover)]:text-gray-600 md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent"
               >
                 About
               </a>
@@ -55,7 +55,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-white rounded [&:not(:hover)]:text-gray-600 md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent"
               >
                 Services
               </a>
@@ -63,7 +63,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-white rounded [&:not(:hover)]:text-gray-600 md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent"
               >
                 Pricing
               </a>
@@ -71,7 +71,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-white rounded [&:not(:hover)]:text-gray-600 md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent"
               >
                 Contact
               </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
 import Image from "next/image";
 import Footer from "./footer";
 import Navbar from "./navbar";
+import Mask from "../app/svg-components/mask";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <main>
-        <section className="flex">
-          <div
-            className="w-0 h-0
-          md:border-l-[25rem] border-l-blue-500 
-  md:border-t-[25rem] border-t-transparent
-  border-b-[0px] border-b-transparent"
-          ></div>
-          <div
-            className="w-0 h-0 
-  border-t-[25rem]
-  border-r-[25rem] border-r-green-500
-   border-t-transparent
-  border-b-[0px] border-b-transparent"
-          ></div>
+        <section className="flex justify-center items-center bg-[url('../public/mask-blue.svg')] bg-no-repeat bg-[length:100%] bg-center mask">
+          <div className="grid justify-items-stretch">
+            <div className="justify-self-center font-black sm:text-lg md:text-4xl lg:text-9xl">
+              <div className="grid justify-items-center">
+                <span className="text-white justify-self-center">RAN</span>
+                <span className="text-white -mt-6">VARGAS</span>
+              </div>
+            </div>
+            <p className="font-bold text-white mt-2 sm:text-base md:text-lg lg:text-2xl">
+              SOFTWARE ENGINEER, BACKEND, SYSTEMS & BLOCKCHAIN DEVELOPER.
+            </p>
+          </div>
+        </section>
+        <section>
+          <span className="text-white">My Expertise</span>
         </section>
       </main>
       <Footer />
