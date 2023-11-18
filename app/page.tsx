643bfec45b7028fc062a6ec479163cda51ac6eac
RanVargas <Ren-elias@hotmail.com>
2023-11-18 02:44:23 -0400

    Adjusted text size

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
         <section className="flex justify-center items-center bg-[url('../public/mask-blue.svg')] bg-no-repeat bg-[length:100%] bg-center mask">
           <div className="grid justify-items-stretch">
             <div className="justify-self-center font-black sm:text-lg md:text-4xl lg:text-9xl">
-              <div className="grid justify-items-center">
+              <div className="md:grid justify-items-center sm:flex">
                 <span className="text-white justify-self-center">RAN</span>
                 <span className="text-white -mt-6">VARGAS</span>
               </div>
             </div>
-            <p className="font-bold text-white mt-2 sm:text-base md:text-lg lg:text-2xl">
+            <p className="font-bold text-white mt-2  sm:text-xs md:text-lg lg:text-2xl">
               SOFTWARE ENGINEER, BACKEND, SYSTEMS & BLOCKCHAIN DEVELOPER.
             </p>
           </div>
